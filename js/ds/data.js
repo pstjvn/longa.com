847de04a25142c00d321ff84489364fdc861bead
Peter StJ <regardingscot@gmail.com>
2015-06-10 16:51:56 +0300

    Fix file name.

--- js/ds/data.js
+++ js/ds/data.js
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview Provides single place for the global data instances that
+ * are expected to be read by data utilities and components / controls.
+ */
+
 goog.provide('longa.data');
 
 goog.require('longa.ds.Alerts');
@@ -5,6 +10,7 @@ goog.require('longa.gen.dto.Profile');
 goog.require('longa.gen.dto.Sellers');
 goog.require('longa.gen.dto.Service');
 goog.require('longa.gen.dto.User');
+goog.require('longa.gen.dto.UserBalance');
 
 
 /**
@@ -19,36 +25,9 @@ longa.data.user = new longa.gen.dto.User();
  *
  * When no user is logged in it will be null.
  *
- * @type {longa.gen.dto.UserBalance}
+ * @type {?longa.gen.dto.UserBalance}
  */
 longa.data.balance = null;
-
-
-/**
- * Configure the images that you want preloaded.
- * @type {Array<string>}
- */
-longa.data.preloadImages = [
-  'assets/images/bull.jpg',
-  'assets/images/b.jpg',
-  'assets/images/c.jpg',
-  'assets/images/hands.jpg',
-  'assets/images/d.jpg'
-];
-
-
-/**
- * The images used by the swpier in FAQ page.
- * @type {Array<string>}
- */
-longa.data.preloadFaqImages = [
-  'assets/images/1.png',
-  'assets/images/2.jpg',
-  'assets/images/3.jpg',
-  'assets/images/4.jpg',
-  'assets/images/5.jpg',
-  'assets/images/6.jpg'
-];
 
 
 /**
